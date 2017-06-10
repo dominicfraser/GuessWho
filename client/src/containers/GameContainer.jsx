8d0fd82e8d74112238811126b14afb35c62a6112
Dominic <mrdjfraser@gmail.com>
2017-06-10 15:56:10 +0100

    grid of characters created

--- client/src/containers/GameContainer.jsx
+++ client/src/containers/GameContainer.jsx
@@ -5,14 +5,26 @@ import CharacterTile from '../components/CharacterTile'
 class GameContainer extends React.Component {
   constructor(props){
     super(props)
-    this.state = {}
+    this.state = {
+      possibleChars: ['antman', 'batman', 'blackcanary', 'blackwidow', 'captainamerica', 'catwoman', 'cyclops', 'flash', 'greenarrow', 'hawkgirl', 'hulk', 'ironman', 'spiderman', 'starlord', 'superman', 'thor', 'wolverine', 'wonderwoman']
+    }
+
   }
 
   render(){
+    const tiles = this.state.possibleChars.map((char, index) => {
+      const srcPath = `./public/images/${char}.png`
+      return (
+        <CharacterTile index={index} key={index} src={srcPath} />
+        ) 
+    })
+
     return (
         <div className='game-container'>
           <Header heading="Guess Who" />
-          <CharacterTile src='./public/images/batman.png' />
+          <div className='character-tiles-grid'>
+            {tiles}
+          </div>
         </div>
       )
   }
